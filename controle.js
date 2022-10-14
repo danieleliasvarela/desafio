298c0da0a9eb8dd8e255ab12da51b2a64719cc4f
Daniel Varela <daniel.varela_@hotmail.com>
2022-10-13 22:28:26 -0300

    resposta baseada no status

--- controle.js
+++ controle.js
@@ -9,36 +9,40 @@ formulario.controller('controlador_formulario', function($scope, $http){
     $scope.sobrenome = "";
     $scope.resposta = "";
 
+
+
     // FUNCAO postar_dados
     $scope.postar_dados = function(nome, sobrenome, resposta){  // FUNCAO PARA POSTAR DADOS QUE VEM LA DO HTML
         
-        // CRIEI VARIAVEL DADOS PARA ARMAZENAR TUDO QUE FOI PASSADO PELO HTML
+
+        
+        // CRIEI VARIAVEL DADOS PARA ARMAZENAR TUDO QUE FOI PASSADO PELO HTML 
         var dados = {   
             nome: $scope.nome,
             sobrenome: $scope.sobrenome,
             resposta: $scope.resposta
-        }   // FIM DA VARIAVEL dados
+        }
+
+
 
         // METODO POST TRANSFORMANDO dados EM JSON PARA PODER PASSAR
-        $http.post('https://63429207ba4478d4783dd7a1.mockapi.io/desafio/dados', JSON.stringify(dados)).then(function(response) {});
+        $http.post('https://63429207ba4478d4783dd7a1.mockapi.io/desafio/dados', JSON.stringify(dados))
+        .then(function(response){
+            $scope.status_do_metodo = response.statusText; // TEXTO INDICANDO STATUS DO METODO EH PASSADO PARA A VARIAVEL $scope.statustext
 
-        // CONDICIONAL CASO A RESPOSTA SEJA POSITIVA
+            // CONDICIONAL CASO A O STATUS DO METODO SEJA POSITIVO (RESPOSTA POSITIVA PRO "POST")
+            if ($scope.status_do_metodo == "Created")
+            {
+                alert('Você está mais próximo de se juntar ao time!');
+            }
+            else
+            {
+                alert('Erro');
+            }
 
-        var r = $scope.resposta;
-
-        if (r == 'sim' || r == 'Sim' || r == 'SIM') 
-        {
-            alert('Sucesso');
-        }
-        else 
-        {
-            alert('Erro');
-        }
-    
+        });                     // fim do then
     }
 
-});
-
-
+});                             // fim do controller
 
 
